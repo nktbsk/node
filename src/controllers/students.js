e2846d779bcd520bc64cf2c59ac26cc65e1f99ab
nktbsk <nikita.bashkir01@gmail.com>
2024-12-05 12:22:18 +0200

    3 module

---
+++ src/controllers/students.js
@@ -0,0 +1,96 @@
+import createHttpError from 'http-errors';
+
+import {
+  getAllStudents,
+  getStudentById,
+  createStudent,
+  deleteStudent,
+  updateStudent,
+} from '../services/students.js';
+
+export const getStudentsController = async (req, res) => {
+  const students = await getAllStudents();
+
+  res.status(200).json({
+    status: 200,
+    message: 'Successfully found students!',
+    data: students,
+  });
+};
+
+export const getStudentByIdController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const student = await getStudentById(studentId);
+
+  // Відповідь, якщо контакт не знайдено
+  if (!student) {
+    throw createHttpError(404, 'Student not found');
+  }
+
+  // Відповідь, якщо контакт знайдено
+  res.status(200).json({
+    status: 200,
+    message: `Successfully found student with id ${studentId}!`,
+    data: student,
+  });
+};
+
+export const createStudentController = async (req, res) => {
+  const student = await createStudent(req.body);
+
+  res.status(201).json({
+    status: 201,
+    message: 'Successfully created student!',
+    data: student,
+  });
+};
+
+export const deleteStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const student = await deleteStudent(studentId);
+
+  if (!student) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.status(204).send();
+};
+
+export const upsertStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+
+  const result = await updateStudent(studentId, req.body, {
+    upsert: true,
+  });
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  const status = result.isNew ? 201 : 200;
+
+  res.status(status).json({
+    status,
+    message: `Successfully upserted a student!`,
+    data: result.student,
+  });
+};
+
+export const patchStudentController = async (req, res, next) => {
+  const { studentId } = req.params;
+  const result = await updateStudent(studentId, req.body);
+
+  if (!result) {
+    next(createHttpError(404, 'Student not found'));
+    return;
+  }
+
+  res.json({
+    status: 200,
+    message: `Successfully patched a student!`,
+    data: result.student,
+  });
+};
---
+++ src/services/students.js
@@ -0,0 +1,42 @@
+import { StudentsCollection } from '../db/models/student.js';
+
+export const getAllStudents = async () => {
+  const students = await StudentsCollection.find();
+  return students;
+};
+
+export const getStudentById = async (studentId) => {
+  const student = await StudentsCollection.findById(studentId);
+  return student;
+};
+
+export const createStudent = async (payload) => {
+  const student = await StudentsCollection.create(payload);
+  return student;
+};
+
+export const deleteStudent = async (studentId) => {
+  const student = await StudentsCollection.findByIdAndDelete({
+    studentId,
+  });
+  return student;
+};
+
+export const updateStudent = async (studentId, payload, options = {}) => {
+  const rawResult = await StudentsCollection.findOneAndUpdate(
+    studentId,
+    payload,
+    {
+      new: true,
+      includeResultMetadata: true,
+      ...options,
+    },
+  );
+
+  if (!rawResult || !rawResult.value) return null;
+
+  return {
+    student: rawResult.value,
+    isNew: Boolean(rawResult?.lastErrorObject?.upserted),
+  };
+};
