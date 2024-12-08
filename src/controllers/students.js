86d8ae05a5133392d2c3158d3a25a13af8aa4c0d
nktbsk <nikita.bashkir01@gmail.com>
2024-12-08 12:55:17 +0200

    4 module

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -7,11 +7,24 @@ import {
   deleteStudent,
   updateStudent,
 } from '../services/students.js';
+import { parsePaginationParams } from '../utils/parsePaginationParams.js';
+import { parseSortParams } from '../utils/parseSortParams.js';
+import { parseFilterParams } from '../utils/parseFilterParams.js';
 
 export const getStudentsController = async (req, res) => {
-  const students = await getAllStudents();
+  const { page, perPage } = parsePaginationParams(req.query);
+  const { sortBy, sortOrder } = parseSortParams(req.query);
+  const filter = parseFilterParams(req.query);
 
-  res.status(200).json({
+  const students = await getAllStudents({
+    page,
+    perPage,
+    sortBy,
+    sortOrder,
+    filter,
+  });
+
+  res.json({
     status: 200,
     message: 'Successfully found students!',
     data: students,
